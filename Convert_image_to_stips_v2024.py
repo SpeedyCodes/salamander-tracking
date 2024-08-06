21a67be45c9e1cf5e0157468a752f1db71cc31e8
RuneDC333 <runedeconinck@outlook.be>
2024-08-06 08:47:11 +0200

    Using full image
    Now the code is corrected and the full image is returned. The dots are marked on the full image.

--- Convert_image_to_stips_v2024.py
+++ Convert_image_to_stips_v2024.py
@@ -10,6 +10,7 @@ import cv2 as cv
 from Convert_image_to_stips import filenames_from_folder, resize_with_aspect_ratio
 from utils.heic_imread_wrapper import wrapped_imread
 import numpy as np
+import copy
 
 
 def convert_image_to_stips_v2024(image: np.ndarray, is_show: bool = False) -> set[tuple[int, int, int, int, bool]]:
@@ -23,36 +24,38 @@ def convert_image_to_stips_v2024(image: np.ndarray, is_show: bool = False) -> se
     The dots are being stored as rectangles where (x, y) are the coordinates of the top left corner of the dot.
     """
 
-    image, image_gray = preprocess_image(image, is_crop=True)
-    list_of_dots = dot_detection(image, image_gray, min_area=20, max_area=2500)
+    image_full = copy.copy(image)
+    image, image_gray, x_new, y_new = preprocess_image(image)
+    list_of_dots = dot_detection(image, image_gray, x_new, y_new, min_area=20, max_area=2500)
 
     if is_show:
-        show_image(image, list_of_dots)
+        show_image(image_full, list_of_dots)
 
     return list_of_dots
 
 
-def preprocess_image(image: np.ndarray, is_crop: bool = True):
+def preprocess_image(image: np.ndarray):
     """ This method preprocesses the given image. This includes isolating the salamander from the background.
     Thresholding and blurring and trying to preserve as many good edges as possible. """
 
     image = isolate_salamander(image)
 
-    if is_crop:
-        image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-        x, y, w, h = cv.boundingRect(image_gray)
-        image = image[y: y + h, x: x + w]
+    image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    # x and y denote the linear transformation to the new origin after cropping.
+    x, y, w, h = cv.boundingRect(image_gray)
+    image = image[y: y + h, x: x + w]
 
     image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     _, image = cv.threshold(image_gray, 50, 255, cv.THRESH_BINARY)
 
     image = cv.medianBlur(image, 9)
 
-    return image, image_gray
+    return image, image_gray, x, y
 
 
-def dot_detection(image: np.ndarray, image_gray: np.ndarray, min_area, max_area):
-    """ This method will detect the dots on an image of a salamander and label the dots as good dots of false dots."""
+def dot_detection(image: np.ndarray, image_gray: np.ndarray, x_new: int, y_new: int, min_area, max_area):
+    """ This method will detect the dots on an image of a salamander and label the dots as good dots of false dots.
+    x_new and y_new denote the location of the new origin (after cropping) in regard to the original origin."""
 
     # Detect the largest contour in the image; this will be the body of the salamander.
     large_contours, _ = cv.findContours(image_gray, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
@@ -98,7 +101,7 @@ def dot_detection(image: np.ndarray, image_gray: np.ndarray, min_area, max_area)
                     is_good_dot = True
                 else:
                     is_good_dot = False
-                list_of_dots.add((x, y, w, h, is_good_dot))
+                list_of_dots.add((x + x_new, y + y_new, w, h, is_good_dot))
 
     return list_of_dots
 
@@ -106,7 +109,6 @@ def dot_detection(image: np.ndarray, image_gray: np.ndarray, min_area, max_area)
 def show_image(image: np.ndarray, list_of_dots):
     """ This method will show the given image with dots drawn on it. """
 
-    image = cv.cvtColor(image, cv.COLOR_GRAY2BGR)
     for dot in list_of_dots:
         x, y, w, h, is_good_dot = dot
 
