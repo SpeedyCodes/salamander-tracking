9175501db147d5d53321ae6317ac5d4d1827cf1b
Jesse Daems <42868171+SpeedyCodes@users.noreply.github.com>
2024-08-29 00:02:55 +0200

    update facade to compare remapped coords

--- src/facade.py
+++ src/facade.py
@@ -1,8 +1,7 @@
 import numpy as np
-from src.pose_estimation.estimate import estimate_pose_from_image
+from src.pose_estimation import estimate_pose_from_image, CoordinateTransformer
 from src.dot_detection import dot_detect_haar
-from src.preprocessing import normalise_coordinates
-from src.preprocessing.isolate_salamander import crop_image
+from src.preprocessing import normalise_coordinates, crop_image
 from src.pattern_matching import compare_dot_patterns
 
 """
@@ -11,75 +10,93 @@ Internally, it defines from a high level the pipeline of operations that need to
 
 """
 
+class CoordinateExtractor:
 
-def crop_image_to_belly(image: np.ndarray) -> np.ndarray:
-    """
-    :param image:
-    :return: A cropped version of the image. In this cropped version, we only see the belly of the salamander,
-    or the whole salamander if so wished.
-    """
+    def __init__(self, image: np.ndarray):
+        self.coordinates_pose = None
+        self.pose_estimation_success = None
+        self.image = image
 
-    # First, try to use pose estimation.
-    pose_estimation_evaluation = 2
-    is_background_removed = False
-    no_background = None
-    cropped_image = None
+    def crop_image_to_belly(self) -> np.ndarray:
+        """
+        :return: A cropped version of the image. In this cropped version, we only see the belly of the salamander,
+        or the whole salamander if so wished.
+        """
 
-    coordinates_pose, succes = estimate_pose_from_image(image)
+        # First, try to use pose estimation.
+        pose_estimation_evaluation = 2
+        is_background_removed = False
+        no_background = None
+        cropped_image = None
 
-    if not succes:
-        # Try to crop the image such that it is smaller.
-        pose_estimation_evaluation -= 1
+        self.coordinates_pose, self.pose_estimation_success = estimate_pose_from_image(self.image)
 
-    if pose_estimation_evaluation == 1:
-        try:
-            # Remove the background.
-            no_background = crop_image(image, coordinates_pose, False, pose_estimation_evaluation)
-        except AssertionError:
-            pose_estimation_evaluation = 0
-        else:
-            is_background_removed = True
-            pose_estimation_evaluation += 1
+        if not self.pose_estimation_success:
+            # Try to crop the image such that it is smaller.
+            pose_estimation_evaluation -= 1
 
-    if pose_estimation_evaluation == 2 and is_background_removed:
-        # Try pose estimation again on smaller image.
-        coordinates_pose, succes = estimate_pose_from_image(no_background)
-
-        if not succes:
-            pose_estimation_evaluation = 0
-
-    if pose_estimation_evaluation == 2:
-        # Crop by using pose_estimation.
-        if is_background_removed:
-
+        if pose_estimation_evaluation == 1:
             try:
-                cropped_image = crop_image(no_background, coordinates_pose, is_background_removed,
-                                           pose_estimation_evaluation)
+                # Remove the background.
+                no_background = crop_image(self.image, self.coordinates_pose, False, pose_estimation_evaluation)
             except AssertionError:
                 pose_estimation_evaluation = 0
+            else:
+                is_background_removed = True
+                pose_estimation_evaluation += 1
 
-        else:
-            try:
-                cropped_image = crop_image(image, coordinates_pose, is_background_removed, pose_estimation_evaluation)
-            except AssertionError:
+        if pose_estimation_evaluation == 2 and is_background_removed:
+            # Try pose estimation again on smaller image.
+            self.coordinates_pose, self.pose_estimation_success = estimate_pose_from_image(no_background)
+
+            if not self.pose_estimation_success:
                 pose_estimation_evaluation = 0
 
-    if pose_estimation_evaluation == 0:
-        # Use the old methods to crop the image.
-        if is_background_removed:
-            try:
-                cropped_image = crop_image(no_background, coordinates_pose, is_background_removed,
-                                           pose_estimation_evaluation)
-            except AssertionError:
-                cropped_image = no_background
+        if pose_estimation_evaluation == 2:
+            # Crop by using pose_estimation.
+            if is_background_removed:
 
-        else:
-            try:
-                cropped_image = crop_image(image, coordinates_pose, is_background_removed, pose_estimation_evaluation)
-            except AssertionError:
-                cropped_image = image
+                try:
+                    cropped_image = crop_image(no_background, self.coordinates_pose, is_background_removed,
+                                               pose_estimation_evaluation)
+                except AssertionError:
+                    pose_estimation_evaluation = 0
 
-    return cropped_image
+            else:
+                try:
+                    cropped_image = crop_image(self.image, self.coordinates_pose, is_background_removed, pose_estimation_evaluation)
+                except AssertionError:
+                    pose_estimation_evaluation = 0
+
+        if pose_estimation_evaluation == 0:
+            # Use the old methods to crop the image.
+            if is_background_removed:
+                try:
+                    cropped_image = crop_image(no_background, self.coordinates_pose, is_background_removed,
+                                               pose_estimation_evaluation)
+                except AssertionError:
+                    cropped_image = no_background
+
+            else:
+                try:
+                    cropped_image = crop_image(self.image, self.coordinates_pose, is_background_removed, pose_estimation_evaluation)
+                except AssertionError:
+                    cropped_image = self.image
+
+        return cropped_image
+
+    def extract(self) -> set[tuple[float, float]]:
+        """
+        :return: The canonical representation of the coordinates of the dots on the salamander's skin in the image.
+        """
+        cropped_image = self.crop_image_to_belly()
+        list_haar_cascade = dot_detect_haar(cropped_image)
+        list_coordinates = normalise_coordinates(list_haar_cascade, cropped_image.shape)
+        if self.pose_estimation_success:
+            coordinate_transformer = CoordinateTransformer(self.coordinates_pose)
+            list_coordinates = set([coordinate_transformer.transform(*coordinate) for coordinate in list_coordinates])
+
+        return list_coordinates
 
 
 def image_to_canonical_representation(image: np.ndarray) -> set[tuple[float, float]]:
@@ -88,11 +105,9 @@ def image_to_canonical_representation(image: np.ndarray) -> set[tuple[float, flo
     :param:  image
     :return: The canonical representation of the coordinates of the dots on the salamander's skin in the image.
     """
-    cropped_image = crop_image_to_belly(image)
-    list_haar_cascade = dot_detect_haar(cropped_image)
-    list_coordinates = normalise_coordinates(list_haar_cascade, cropped_image.shape)
+    coordinate_extractor = CoordinateExtractor(image)
 
-    return list_coordinates
+    return coordinate_extractor.extract()
 
 
 def match_canonical_representation_to_database(canonical_representation: set[tuple[float, float]]) -> str | None:
