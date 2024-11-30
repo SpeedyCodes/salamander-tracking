9241db24d188b7614949b33d70b25c5522924712
Jesse Daems <42868171+SpeedyCodes@users.noreply.github.com>
2024-11-30 21:22:11 +0100

    add individual relationship to sighting

--- server/models/sighting.py
+++ server/models/sighting.py
@@ -2,12 +2,17 @@ from server.models import Base
 from sqlalchemy import Integer, Float, DateTime, ARRAY, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 from typing import Optional, Tuple
+from sqlalchemy.orm import relationship
+
+from server.models.individual import Individual
+
 
 class Sighting(Base):
     __tablename__ = 'sightings'
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, init=False)
     individual_id: Mapped[Optional[int]] = mapped_column(ForeignKey('individuals.id'), nullable=True)
+    individual: Mapped["Individual"] = relationship()
     coordinates: Mapped[list[Tuple[float, float]]] = mapped_column(ARRAY(Float))
     date: Mapped[Optional[DateTime]] = mapped_column(DateTime, nullable=True, init=False)
     image_id: Mapped[Optional[int]] = mapped_column(ForeignKey('image_pipelines.id'), nullable=True)
