607f955d4145d0f13d41fe0b0b6ad989c76b18df
Jesse Daems <jesse.daems@student.uantwerpen.be>
2025-04-16 13:14:10 +0200

    flask_restx for swagger and multiple methods per endpoint, add PUT for individuals and sightings

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, Response, render_template, jsonify
 import cv2
 import numpy as np
-
+from flask_restx import Api, Resource
 from server.models.image_pipeline import ImagePipeline
 from server.models import Base
 from server.models.named_location import NamedLocation
@@ -23,6 +23,9 @@ app = Flask(__name__, static_folder='static', static_url_path='')
 app.config['SQLALCHEMY_DATABASE_URI'] = PG_CONNECTION_STRING
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+api = Api(app, version='1.0', title='Salamander API',
+            description='API for the Salamander recognition system',)
+
 db.init_app(app)
 migrate = Migrate(app, db)
 
@@ -36,193 +39,233 @@ def decode_image(bytestring: bytes):
     return cv2.imdecode(image, cv2.IMREAD_COLOR)
 
 
-@app.route('/store_sighting', methods=['POST'])
-def recognize():
-    """
-    Performs recognition and returns candidates with their confidence levels.
-    The submitted image is saved as a sighting to be confirmed later.
-    """
+@api.route('/store_sighting')
+class Recognize(Resource):
+    def post(self):
+        """
+        Performs recognition and returns candidates with their confidence levels.
+        The submitted image is saved as a sighting to be confirmed later.
+        """
 
-    location_id = request.args.get('location_id', type=int)
-    image_bytes = request.data
-    image = decode_image(image_bytes)
-    coordinates, quality, intermediates = image_to_canonical_representation(image)
-    if quality == ImageQuality.BAD: # if the image is too bad to process, return a 400 and don't store the image
-        return Response(status=400)
-
-
-    candidates = match_canonical_representation_to_database(coordinates, 4, location_id)
-
-    converted_list = [{
-        "confidence": candidate[2],
-        "sighting": get_sighting(int(candidate[4]))
-    }
-        for candidate in candidates]
-    i = 0
-    while i < len(converted_list):
-        sighting = converted_list[i]["sighting"]
-        if sighting.individual_id is None:
-            converted_list.pop(i)
-            continue
-        # filter out all but the first candidate of every individual_id
-        # also, remove candidates that have no individual_id
-        j = i + 1
-        while j < len(converted_list):
-            if sighting.individual_id == converted_list[j]["sighting"].individual_id or converted_list[j]["sighting"].individual_id is None:
-                converted_list.pop(j)
-            else:
-                j += 1
-
-        converted_list[i]["individual"] = get_individual(converted_list[i]["sighting"].individual_id)
-        i += 1
-
-    pose_estimation_image = encode_image(intermediates[0]) if intermediates[0] is not None else None
-    cropped_image = encode_image(intermediates[1]) if intermediates[1] is not None else None
-    dot_detection_image = encode_image(intermediates[2]) if intermediates[2] is not None else None
-    straightened_dots_image = encode_image(intermediates[3]) if intermediates[3] is not None else None
-    images = ImagePipeline(original_image=image_bytes, pose_estimation_image=pose_estimation_image, cropped_image=cropped_image, dot_detection_image=dot_detection_image, straightened_dots_image=straightened_dots_image)
-    store_dataclass(images)
-    sighting = Sighting(individual_id=None, image_id=images.id, coordinates=list(coordinates))
-    sighting = store_dataclass(sighting)
-    return {
-        "sighting_id": sighting.id,
-        "candidates": converted_list,
-        "quality": quality.name
-    }
+        location_id = request.args.get('location_id', type=int)
+        image_bytes = request.data
+        image = decode_image(image_bytes)
+        coordinates, quality, intermediates = image_to_canonical_representation(image)
+        if quality == ImageQuality.BAD: # if the image is too bad to process, return a 400 and don't store the image
+            return Response(status=400)
 
 
-@app.route('/confirm/<string:sighting_id>', methods=['POST'])
-def confirm(sighting_id):
-    """
-    Confirms the identity of the sighting with the given sighting_id,
-    or creates a new individual if the salamander is not in the database.
-    """
-    individual_id = request.args.get('individual_id', type=int)
-    location_id = request.args.get('location_id', type=int)
-    body = request.json
-    sighting = get_sighting(sighting_id)
-    if not individual_id:  # if the individual is new
-        individual = Individual(name=body["nickname"])
-        individual = store_dataclass(individual)
-    else:
-        individual = db.session.get(Individual, individual_id)
+        candidates = match_canonical_representation_to_database(coordinates, 4, location_id)
 
-    if location_id:
-        sighting.location_id = location_id
-    sighting.individual_id = individual.id
-    sighting.date = datetime.strptime(body["spotted_at"], "%Y-%m-%dT%H:%M:%S.%f")
-    db.session.commit()
-    return Response(status=200)
+        converted_list = [{
+            "confidence": candidate[2],
+            "sighting": get_sighting(int(candidate[4]))
+        }
+            for candidate in candidates]
+        i = 0
+        while i < len(converted_list):
+            sighting = converted_list[i]["sighting"]
+            if sighting.individual_id is None:
+                converted_list.pop(i)
+                continue
+            # filter out all but the first candidate of every individual_id
+            # also, remove candidates that have no individual_id
+            j = i + 1
+            while j < len(converted_list):
+                if sighting.individual_id == converted_list[j]["sighting"].individual_id or converted_list[j]["sighting"].individual_id is None:
+                    converted_list.pop(j)
+                else:
+                    j += 1
 
+            converted_list[i]["individual"] = get_individual(converted_list[i]["sighting"].individual_id)
+            i += 1
 
-@app.route('/individuals/<string:id>', methods=['GET'])
-def info(id):
-    """
-    Returns the information of the salamander with the given id.
-    """
-
-    return jsonify(get_individual(id))
-
-
-@app.route('/individuals', methods=['GET'])
-def all_individuals():
-    """
-    Returns the information of all the salamanders in the database.
-    """
-    location_id = request.args.get('location_id', type=int)
-    return get_individuals(location_id)
+        pose_estimation_image = encode_image(intermediates[0]) if intermediates[0] is not None else None
+        cropped_image = encode_image(intermediates[1]) if intermediates[1] is not None else None
+        dot_detection_image = encode_image(intermediates[2]) if intermediates[2] is not None else None
+        straightened_dots_image = encode_image(intermediates[3]) if intermediates[3] is not None else None
+        images = ImagePipeline(original_image=image_bytes, pose_estimation_image=pose_estimation_image, cropped_image=cropped_image, dot_detection_image=dot_detection_image, straightened_dots_image=straightened_dots_image)
+        store_dataclass(images)
+        sighting = Sighting(individual_id=None, image_id=images.id, coordinates=list(coordinates))
+        sighting = store_dataclass(sighting)
+        return {
+            "sighting_id": sighting.id,
+            "candidates": converted_list,
+            "quality": quality.name
+        }
 
 
-@app.route('/individuals/<string:id>/image', methods=['GET'])
-def individual_image(id):
-    """
-    Returns an image of the salamander with the given id.
-    """
-    individual = db.session.get(Individual, id)
-    sighting = db.session.query(Sighting).filter(Sighting.individual_id == individual.id).first()
-    image = db.session.get(ImagePipeline, sighting.image_id).original_image
-    return Response(image, mimetype='image/png')
+@api.route('/confirm/<string:sighting_id>')
+class Confirm(Resource):
+    def post(self, sighting_id):
+        """
+        Confirms the identity of the sighting with the given sighting_id,
+        or creates a new individual if the salamander is not in the database.
+        """
+        individual_id = request.args.get('individual_id', type=int)
+        location_id = request.args.get('location_id', type=int)
+        body = request.json
+        sighting = get_sighting(sighting_id)
+        if not individual_id:  # if the individual is new
+            individual = Individual(name=body["nickname"])
+            individual = store_dataclass(individual)
+        else:
+            individual = db.session.get(Individual, individual_id)
+
+        if location_id:
+            sighting.location_id = location_id
+        sighting.individual_id = individual.id
+        sighting.date = datetime.strptime(body["spotted_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        db.session.commit()
+        return Response(status=200)
 
 
-@app.route('/sightings/<string:id>/image', methods=['GET'])
-def sighting_image(id):
-    """
-    Returns the image of the sighting with the given id.
-    """
 
-    sighting: Sighting = get_sighting(id)
-    image = db.session.get(ImagePipeline, sighting.image_id).original_image
-    return Response(image, mimetype='image/png')
+@api.route('/individuals/<string:individual_id>')
+class IndividualCRUD(Resource):
+    def get(self, individual_id):
+        """
+        Returns the information of the salamander with the given id.
+        """
 
-@app.route('/sightings/<string:id>/image/<string:intermediate_image_name>', methods=['GET'])
-def intermediate_image(id, intermediate_image_name):
-    """
-    Returns the intermediate image of the sighting with the given id.
-    """
+        return jsonify(get_individual(individual_id))
 
-    sighting: Sighting = get_sighting(id)
-    pipeline = db.session.get(ImagePipeline, sighting.image_id)
-    match intermediate_image_name:
-        case "pose_estimation":
-            image = pipeline.pose_estimation_image
-        case "cropped":
-            image = pipeline.cropped_image
-        case "dot_detection":
-            image = pipeline.dot_detection_image
-        case "straightened_dots":
-            image = pipeline.straightened_dots_image
-        case _:
-            return Response(status=404)
-    return Response(image, mimetype='image/png')
+    def put(self, individual_id):
+        """
+        Updates the information of the salamander with the given id.
+        """
+        body = request.json
+        individual = get_individual(individual_id)
+        if "name" in body:
+            individual.name = body["name"]
+        db.session.commit()
+        return individual
 
-@app.route('/sightings', methods=['GET'])
-def get_all_sightings():
-    """
-    Returns the information of all the sightings in the database.
-    """
 
-    location_id = request.args.get('location_id', type=int)
-    individual_id = request.args.get('individual_id', type=int)
-    return get_sightings(individual_id, location_id)
 
-@app.route('/sightings/<string:id>', methods=['DELETE'])
-def delete_sighting(id):
-    """
-    Deletes the sighting with the given id.
-    """
+@api.route('/individuals')
+class IndividualList(Resource):
+    def get(self):
+        """
+        Returns the information of all the salamanders in the database.
+        """
+        location_id = request.args.get('location_id', type=int)
+        return get_individuals(location_id)
 
-    sighting = get_sighting(id)
-    db.session.delete(sighting)
-    db.session.commit()
-    return Response(status=200)
 
-@app.route('/locations', methods=['POST'])
-def add_location():
-    """
-    Adds a location to the database.
-    """
-    body = request.json
-    location = NamedLocation(name=body["name"], precise_location=body["precise_location"])
-    location = store_dataclass(location)
-    return asdict(location)
+@api.route('/individuals/<string:id>/image')
+class IndividualImage(Resource):
+    def get(self, id):
+        """
+        Returns an image of the salamander with the given id.
+        """
+        individual = db.session.get(Individual, id)
+        sighting = db.session.query(Sighting).filter(Sighting.individual_id == individual.id).first()
+        image = db.session.get(ImagePipeline, sighting.image_id).original_image
+        return Response(image, mimetype='image/png')
 
-@app.route('/locations', methods=['GET'])
-def get_locations():
-    """
-    Returns all the locations in the database.
-    """
-    return db.session.query(NamedLocation).all()
 
-@app.route('/auth', methods=['POST'])
-def auth():
-    password = request.json["password"]
-    hashed_passwords = db.session.query(Password).all()
-    if not any(bcrypt.checkpw(password.encode('utf-8'), hashed_password.password.encode('utf-8')) for hashed_password in hashed_passwords):
-        return Response(status=401)
+@api.route('/sightings/<string:id>/image')
+class SightingImage(Resource):
+    def get(self, id):
+        """
+        Returns the image of the sighting with the given id.
+        """
 
-    exp = datetime.utcnow() + timedelta(days=1)
+        sighting: Sighting = get_sighting(id)
+        image = db.session.get(ImagePipeline, sighting.image_id).original_image
+        return Response(image, mimetype='image/png')
 
-    return jwt.encode({'exp': exp}, jwt_secret, algorithm='HS256')
+@api.route('/sightings/<string:id>/image/<string:intermediate_image_name>')
+class IntermediateImage(Resource):
+    def get(self, id, intermediate_image_name):
+        """
+        Returns the intermediate image of the sighting with the given id.
+        """
+
+        sighting: Sighting = get_sighting(id)
+        pipeline = db.session.get(ImagePipeline, sighting.image_id)
+        match intermediate_image_name:
+            case "pose_estimation":
+                image = pipeline.pose_estimation_image
+            case "cropped":
+                image = pipeline.cropped_image
+            case "dot_detection":
+                image = pipeline.dot_detection_image
+            case "straightened_dots":
+                image = pipeline.straightened_dots_image
+            case _:
+                return Response(status=404)
+        return Response(image, mimetype='image/png')
+
+@api.route('/sightings')
+class GetAllSightings(Resource):
+    def get(self):
+        """
+        Returns the information of all the sightings in the database.
+        """
+
+        location_id = request.args.get('location_id', type=int)
+        individual_id = request.args.get('individual_id', type=int)
+        return get_sightings(individual_id, location_id)
+
+@api.route('/sightings/<string:sighting_id>')
+class SightingCRUD(Resource):
+
+    def put(self, sighting_id):
+        """
+        Updates the sighting with the given id.
+        """
+        body = request.json
+        sighting = get_sighting(sighting_id)
+        if "individual_id" in body:
+            sighting.individual_id = body["individual_id"]
+        if "location_id" in body:
+            sighting.location_id = body["location_id"]
+        if "date" in body:
+            sighting.date = datetime.strptime(body["date"], "%Y-%m-%dT%H:%M:%S.%f")
+        db.session.commit()
+        return sighting
+
+    def delete(self, sighting_id):
+        """
+        Deletes the sighting with the given id.
+        """
+
+        sighting = get_sighting(sighting_id)
+        db.session.delete(sighting)
+        db.session.commit()
+        return Response(status=200)
+
+@api.route('/locations')
+class LocationCRUD(Resource):
+    def post(self):
+        """
+        Adds a location to the database.
+        """
+        body = request.json
+        location = NamedLocation(name=body["name"], precise_location=body["precise_location"])
+        location = store_dataclass(location)
+        return asdict(location)
+
+    def get(self):
+        """
+        Returns all the locations in the database.
+        """
+        locations = db.session.query(NamedLocation).all()
+        return [asdict(location) for location in locations]
+
+@api.route('/auth')
+class Auth(Resource):
+    def post(self):
+        password = request.json["password"]
+        hashed_passwords = db.session.query(Password).all()
+        if not any(bcrypt.checkpw(password.encode('utf-8'), hashed_password.password.encode('utf-8')) for hashed_password in hashed_passwords):
+            return Response(status=401)
+
+        exp = datetime.utcnow() + timedelta(days=1)
+
+        return jwt.encode({'exp': exp}, jwt_secret, algorithm='HS256')
 @app.before_request
 def check_auth():
     if (request.method not in ['GET', 'OPTIONS']) and request.path != '/auth':
@@ -247,9 +290,10 @@ def after_request(response):
     response.headers['Vary'] = 'Origin'
     return response
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+@api.route("/")
+class Frontend(Resource):
+    def index(self):
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
